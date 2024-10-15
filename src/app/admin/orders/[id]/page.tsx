114fb3e989c188e1d6e5db1b7190d063b89f40b2
nnachi kingsley <chimakingsley216@gmail.com>
2024-10-15 19:59:46 +0100

    Update: fix bug

--- src/app/admin/orders/[id]/page.tsx
+++ src/app/admin/orders/[id]/page.tsx
@@ -30,9 +30,6 @@ import {
 } from 'lucide-react';
 import AdminLayout from '../../page';
 
-export const description =
-  'An orders dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. The main area has a list of recent orders with a filter and export button. The main area also has a detailed view of a single order with order details, shipping information, billing information, customer information, and payment information.';
-
 const OrdersDetails = () => {
   return (
     <AdminLayout>
